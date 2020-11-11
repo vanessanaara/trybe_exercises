09c36990499312cd80a48dda61d031b78a9406f5
Vanessa Oliveira <vanessanaara@gmail.com>
2020-11-11 00:02:11 -0300

    Jest - Testes Assíncronos - Exercicio 3 a 6.1

---
+++ bloco_10/dia_2/findAnimalsByName.test.js
@@ -0,0 +1,42 @@
+const Animals = [
+  { name: 'Dorminhoco', age: 1, type: 'Dog' },
+  { name: 'Soneca', age: 2, type: 'Dog' },
+  { name: 'Preguiça', age: 5, type: 'Cat' },
+];
+
+const findAnimalByName = (name) => (
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const animalName = Animals.find((animal) => name === animal.name);
+      if (!animalName) {
+        return reject('Nenhum animal com esse nome!');
+      }
+
+      return resolve(animalName);
+    }, 100);
+  })
+);
+
+const getAnimal = (name) => {
+  return findAnimalByName(name).then(animal => animal)
+};
+
+describe('Testando promise - findAnimalByName', () => {
+  describe('Quando existe o animal com o nome procurado', () => {
+    test('Retorne o objeto do animal', () => {
+      expect.assertions(1);
+      return getAnimal('Dorminhoco').then(animal => {
+        expect(animal).toEqual({ name: 'Dorminhoco', age: 1, type: 'Dog' });
+      });
+    });
+  });
+
+  describe('Quando não existe o animal com o nome procurado', () => {
+    test('Retorna um erro', () => {
+      expect.assertions(1);
+      return getAnimal('Bob').catch(error =>
+        expect(error).toEqual('Nenhum animal com esse nome!')
+      );
+    });
+  });
+});
--- bloco_10/dia_2/getUserName.test.js
+++ bloco_10/dia_2/getUserName.test.js
@@ -26,3 +26,20 @@ test('returs error mesage when user id does not exists', () => {
   expect.assertions(1);
   return getUserName(2).catch(error => expect(error).toEqual({error: 'User with 2 not found.'}));
 });
+
+describe('getUserName test using async/await', () => {
+  test('returns the user name when user id exists', async() => {
+    expect.assertions(1);
+    const response = await getUserName(4);
+    expect(response).toEqual('Mark');
+  });
+
+  test('returs error mesage when user id does not exists', async () => {
+    expect.assertions(1);
+    try {
+      await getUserName(2);
+    } catch(error) {
+      expect(error).toEqual({error: 'User with 2 not found.'});
+    } 
+  });
+});
